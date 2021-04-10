93008e0c7568bad1cb504a6c1880a33bfbf97d36
ElkinMoreno01226 <ingelkinmh@gmail.com>
2021-04-10 11:28:43 -0500

    Ejercicio variables completo

--- 1-Variables/variables.dart
+++ 1-Variables/variables.dart
@@ -2,6 +2,9 @@ void main(){
  String nombre ="Juan";
  int edad = 23;
  String telefono = "3003458961";
+ int ? variableAceptanull = null;
+
+ final String segundoapellido = "Herrera";
 
  var apellido;
  print (nombre);
